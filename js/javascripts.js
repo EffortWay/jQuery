59210eb650f86514621ff050c8141cdc2b223171
EffortWay <effortway@gmail.com>
2022-01-14 16:34:09 +0600

    Mis

--- js/javascripts.js
+++ js/javascripts.js
@@ -1,1 +1,14 @@
 $('.p').hide();
+$('.div-header .img-minus').hide();
+
+function clickButton() {
+	
+	$('.p').slideToggle();
+	$('.div-header .img-plus').toggle();
+	$('.div-header .img-minus').show();
+
+}
+
+
+
+$('.div-header').click(clickButton);
