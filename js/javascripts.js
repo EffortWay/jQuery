0979a9d341040f737c17b5698521980832dcc7d4
EffortWay <effortway@gmail.com>
2022-01-18 19:13:23 +0600

    Accordion Complete With JavaScript

--- js/javascripts.js
+++ js/javascripts.js
@@ -4,12 +4,17 @@ $('.collepse-main .collepse-item .item-head').click(
 		var cam = $(this).closest('.collepse-item')
 
 		cam.toggleClass('active')
-		cam.siblings().removeClass('active').find('.item-collepse').slideUp()
+		cam.siblings().removeClass('active').find('.item-collepse').slideUp().siblings('.item-head').find('.fa-plus').show().siblings('.fa-minus').hide();
+
 		if( cam.hasClass('active') ) {
+
 			cam.find('.item-collepse').slideDown()
+			cam.find('.fa-minus').show().siblings('.fa-plus').hide();
+
 		}
 		else {
-			cam.find('.item-collepse').slideUp()
+
+			cam.find('.item-collepse').slideUp().siblings('.item-head').find('.fa-plus').show().siblings('.fa-minus').hide();
 		}
 	}
 
